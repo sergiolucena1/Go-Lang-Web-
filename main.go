26f8e4c3161b7fe95a9129010f0e868b0fbdd33f
Sérgio Lucena <slucenadecarvalho@gmail.com>
2021-07-25 18:28:31 -0300

    creating struct

--- main.go
+++ main.go
@@ -6,6 +6,12 @@ import (
 	"net/http"
 )
 
+type Produtos struct {
+	Nome string
+	Descricao string
+	Preco float64
+	Quantidade int
+}
 
 var temp = template.Must(template.ParseGlob("templates/*.html"))
 //template.Must (encapsula todas as nossas templates e devolve 2 retornos(o template e o erro))
@@ -18,5 +24,12 @@ func main() {
 
 	//Parametros(quem vai escrever, quem vai exibir )
 func index(w http.ResponseWriter, r *http.Request) {
-	temp.ExecuteTemplate(w, "Index", nil)
+	produtos := []Produtos{
+		{Nome: "Camiseta", Descricao: "Azul, bem bonita", Preco: 39, Quantidade: 5},
+		{"Tênis", "Confortável", 89, 3},
+		{"Fone", "Muito bom", 59, 2},
+		{"Produto Novo","Muito legal", 1.99, 1},
+	}
+
+	temp.ExecuteTemplate(w, "Index", produtos)
 }
